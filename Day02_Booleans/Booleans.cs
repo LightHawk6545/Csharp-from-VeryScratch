af1449b0391b898aad12f901ff2429848d27855f
Mobin <136748080+LightHawk6545@users.noreply.github.com>
2025-11-20 16:58:43 +0330

    Add files via upload

---
+++ Day02_Booleans/Booleans.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobin_learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean speical 
+            //My first plain pracrices with seeing where it gets me.
+
+
+            Console.WriteLine("Hey mobin you cleaned the dogs yeah?");
+            Console.ReadLine();
+
+
+            bool Dogs_Clean = true;
+            Console.WriteLine(Dogs_Clean);
+            Console.ReadLine();
+
+
+            Console.WriteLine("... what?");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Am i dumb here? - true? ... what happened to yes and no? are you high?");
+            Console.ReadLine();
+
+
+            Console.WriteLine("*Mysterious mobin proceeds to uncover the answers to each");
+            Console.ReadLine();
+
+
+            bool Is_Dumb = true;
+            bool Is_High = false;
+            bool Is_Low = true;
+
+
+            Console.WriteLine(Is_Dumb);
+            Console.WriteLine(Is_High);
+            Console.WriteLine(Is_Low);
+            Console.ReadLine();
+
+
+            Console.WriteLine("what on god's green is this man true and falsing me like a goddamn side character");
+            Console.ReadLine();
+
+
+            Console.WriteLine("im sure... no. i know. he didn't do weeds when i met him.");
+            Console.ReadLine();
+
+
+            Console.WriteLine("*Mysterious mobin responding to the word weed specifically");
+            Console.ReadLine();
+
+
+            bool On_Weeds = true;
+            Console.WriteLine(On_Weeds);
+            Console.ReadLine();
+
+
+            Console.WriteLine("i had some guesses...");
+            Console.ReadLine();
+
+
+            Console.WriteLine("*The Mobin unfathomably smirk at his bro guessing things");
+            Console.ReadLine();
+
+
+            bool Bro_had_guesses = false;
+            Console.WriteLine(Bro_had_guesses);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Alright man, get out of here, episode is over, go home, much obliged.");
+            Console.ReadLine();
+
+
+            Console.WriteLine("*The mobin doesn't like this over yet tho but whatever.");
+            Console.ReadLine();
+
+
+            bool Episode_Over = true;
+            Console.WriteLine(Episode_Over);
+            Console.ReadLine();
+
+
+            Console.WriteLine("...");
+            Console.ReadLine();
+
+
+            Console.WriteLine("OUT.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Day02_Booleans/String Conversion.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobin_learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Converting strings to numbers session
+            // I'll be practicing with all of numeric data type with a side story (Just the way i love it!!, please don't mind me :)
+            // that means a lot of Console.ReadLine() until i find something better
+
+
+            Console.WriteLine("MOBIN WE'VE GOT BIG FOOT ALERT");
+            Console.WriteLine("actually: man crap this i'll need to use numbers as code then, these big foots be smart");
+            Console.ReadLine();
+
+
+            string evacuation = "662463312";
+            int msg1 = Convert.ToInt32(evacuation);
+            Console.WriteLine(msg1);
+            Console.ReadLine();
+
+
+            Console.WriteLine("This Translate to we have to run.");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Also don't forget to feed the dog at 5:66am...");
+            Console.ReadLine();
+
+
+            string lost = "171717.322444";
+            double msg_2 = Convert.ToDouble(lost);
+            Console.WriteLine(msg_2);
+            Console.ReadLine();
+
+
+            Console.WriteLine("This one is.. bizzare... the crowd got lost with clear signs to the Mount. SD");
+            Console.WriteLine("well... that had to be a mass lost cause case");
+            Console.ReadLine();
+
+
+            string hungry = "25425.22222222";
+            Convert.ToSingle(hungry);
+            Console.WriteLine(hungry);
+            Console.ReadLine();
+
+
+            Console.WriteLine("the big foot actually attacked the food preserves half way to Mount. SD");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Okay he just ate the bacons... and wasted all the other nutrients, well played.");
+            Console.ReadLine();
+
+            string long_way = "-1132222";
+            Convert.ToInt64(long_way);
+            Console.WriteLine(long_way);
+            Console.ReadLine();
+
+
+            Console.WriteLine("ahh well... this is deciphered... and it says they've got way to go past the last forest... might not make it.");
+            Console.ReadLine();
+
+
+            string made_it = "3324165.16111";
+            Convert.ToDecimal(made_it);
+            Console.WriteLine(made_it);
+            Console.ReadLine();
+
+
+            Console.WriteLine("actually some genius found the way and distracted bigfoot by throwing more bacon the other way");
+            Console.WriteLine("The End, credits: Mount. SD");
+            Console.ReadLine();
+
+
+        }
+    }
+}
