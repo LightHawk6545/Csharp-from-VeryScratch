7360618723df3a93d5c6fb3a32e3cab980e47676
Mobin <136748080+LightHawk6545@users.noreply.github.com>
2025-11-24 19:59:46 +0330

    Add files via upload

---
+++ Day05_Switch/For Loop.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Mobin_learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StoryBegins();
+            AtBank();
+            BackHome();
+        }
+
+
+        // An experiment on while loop.
+
+        // A story on withdrawing money from the bank.
+        static void StoryBegins()
+        {
+            Say("Mobin", "Ive got to pay my loan today. ");
+            Say("Mobin", "What do you think coco, will i forget the pin again?");
+
+            Hold();
+
+            Say("Coco", "I mean, its not your card, thats why you forget, try your best tho.");
+            Console.WriteLine();
+            Hold();
+
+            Scene("Mobin leaves to the bank with uncertainty");
+            Scene("After 5 minutes he arrives there");
+            Hold();
+        }
+
+        static void AtBank()
+        {
+            Say("Mobin", "Okay let me try inserting my card in the ATM... (it is 3185)");
+            Hold();
+
+
+            int pin = 0;
+
+            while (pin != 3185)
+            {
+
+
+                Say("ATM", "Please enter pin: ");
+                bool attemptingPin = int.TryParse(Console.ReadLine(), out pin);
+
+            }
+
+            Console.WriteLine("Thank you, here is your 100,000$ cash.");
+            Hold();
+
+
+
+        }
+
+        static void BackHome()
+        {
+            Say("Mobin", "Ah I recently changed the lock... now which is my home key?");
+            Say("Mobin", "There is a blue, red and green key, I guess I'll try each.");
+            Hold();
+            Say("Mobin", "Alright... type a key color:");
+
+            string tryKey = Console.ReadLine();
+
+            while (tryKey != "red")
+            {
+                Say("Mobin", "That's not it...");
+                Say("Mobin", "Let me try another key...");
+
+                tryKey = Console.ReadLine(); 
+            }
+
+            Say("Mobin", "Finally it worked, it's red.");
+            Hold();
+            Say("Mobin", "Alright i can get some rest now.");
+            Hold();
+        }
+
+        static void Hold()
+        {
+            Console.ReadLine();
+        }
+
+        static void Say(string message, string line)
+        {
+            Console.WriteLine($"[{message}] {line}");
+        }
+
+        static void Scene(string scene)
+        {
+            Console.WriteLine($"* {scene} *");
+        }
+
+        static void SayColored(string character, string line, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{character}: {line}");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ Day05_Switch/Switch.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Mobin_learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TheCall();
+            PrintingModels();
+            RecievingPrints();
+            LeavingTheStore();
+        }
+
+        // For Loop mini practice.
+
+        // -- Part 1 --
+        static void TheCall()
+        {
+            Console.WriteLine(" (*Phone starts ring at 9 a.m in the morning*) ");
+            Hold();
+            Console.WriteLine(" *Someone picked up* ");
+            Hold();
+
+            Console.WriteLine("Operator: Hello, this is mary calling from printing center");
+            Console.WriteLine("Your custom 3D models are ready to be printed, we need you here to sign the contract to start printing ");
+            Hold();
+
+            Console.WriteLine("*Mysterious person reveals their voice*");
+            Console.WriteLine("Mobin: Alright thank you very much i'll be there at 10 a.m");
+            Hold();
+        }
+
+        // -- Part 2 --
+        static void PrintingModels()
+        {
+            Console.WriteLine("* A black car parked at printing center at that time *");
+            Console.WriteLine("* Mobin had just arrived there *");
+            Hold();
+
+            Console.WriteLine("Mobin: Hello, im here to sign...");
+            Console.WriteLine("Mary: Weclome, please here and there.");
+            Console.WriteLine("Very good, we can start, you can wait and watch the machine 3D while it operates");
+            Hold();
+
+            Console.WriteLine("Over the next 30 minutes: (The machine logged each succesful print.) ");
+            for (int print = 1; print <= 5; print++)
+            {
+                Console.WriteLine($"3D model number {print}, is ready.");
+                Hold();
+            }
+        }
+        // -- part 3 --
+        static void RecievingPrints()
+        {
+            Console.WriteLine("Mobin: okay that is all of the models, how can i pay?");
+            Console.WriteLine("Mary: Cash please, it will be 100 dollars");
+            Hold();
+
+            Console.WriteLine("Mobin: Oh looks like im missing 30 dollars, let me search im sure i have it");
+            Console.WriteLine("* Mobin eventually found extra cash and counted them");
+            Hold();
+
+            Console.WriteLine("There i go, i'll count so i don't miss the number");
+            Hold();
+
+            int money = 70;
+            for (int count = money; money <= 100; money += 10)
+            {
+                Console.WriteLine($"That's {money}");
+                Hold();
+
+            }
+        }
+            static void LeavingTheStore()
+            {
+                Console.WriteLine("Mary: Thank you very much sir, that will do.");
+                Console.WriteLine("Have a good day.");
+                Console.WriteLine("Mobin: Thank you for your service, cheers.");
+                Hold();
+            }
+
+
+
+        static void Hold()
+        {
+            Console.ReadLine();
+        }
+    }
+}
